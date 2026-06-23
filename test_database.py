e849b28be2afe0e279cf020182de34d8b977b287
DHua03 <dylan31303@gmail.com>
2026-06-23 17:44:49 -0500

    Delete test files

--- test_database.py
+++
@@ -1,9 +0,0 @@
-'''from database import initialize_database, add_transaction, get_transactions, get_balance
-
-initialize_database()
-
-add_transaction("Food", 100, "Test deposit", "deposit")
-add_transaction("Food", -20, "Groceries", "withdraw")
-
-print(get_transactions("Food"))
-print("Balance:", get_balance("Food"))'''
